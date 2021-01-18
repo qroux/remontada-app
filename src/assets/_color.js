69cb5258953b50995d9729ed7d609d3c66923389
Quentin Roux <quentin.roux@hotmail.fr>
2021-01-18 18:15:04 +0100

    initial setup + css theme

---
+++ src/assets/_color.js
@@ -0,0 +1,8 @@
+export const Colors = {
+  primary: "#9893DA",
+  primaryDark: "#797A9E",
+  primaryLight: "#BBBDF6",
+  secondary: "#72727E",
+  secondaryDark: "#625F63",
+  secondaryLight: "#f4f4f5",
+};
