30cd3cc8444899eb97e49e4a5757e5d7792f0b38
Quentin Roux <quentin.roux@hotmail.fr>
2021-01-27 19:40:58 +0100

    Get /Teams endpoint working

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -6,19 +6,38 @@ import {
   ImageBackground,
   Dimensions,
   SafeAreaView,
+  FlatList,
 } from "react-native";
+import strapiApi from "../api/strapiApi";
 import { Button } from "react-native-elements";
 import { Common } from "../assets/common";
 import { Colors, Spacing, Size } from "../assets/main";
 import { Context as AuthContext } from "../context/AuthContext";
 
 export const AccountScreen = () => {
+  const [teams, setTeams] = useState([]);
   const { state, signout } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const response = await strapiApi.get("/teams");
+      setTeams(response.data);
+    };
+
+    fetchTeams();
+  }, []);
   return (
     <SafeAreaView style={Common.fullPage}>
       <View style={Common.container}>
         <Text style={Common.title}>Account Screen</Text>
         <Button title="disconnect" onPress={signout} />
+        <FlatList
+          data={teams}
+          keyExtractor={(team) => team.name}
+          renderItem={({ item }) => {
+            return <Text>{item.name}</Text>;
+          }}
+        />
       </View>
     </SafeAreaView>
   );
