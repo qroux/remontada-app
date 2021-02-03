bd0fbdb17a8ce26a2a2f6971c66e69105c5cb5d1
Quentin Roux <quentin.roux@hotmail.fr>
2021-02-03 19:45:15 +0100

    Refesh controll on PronostricScreen working

--- src/components/mainComponents/MatchList.js
+++ src/components/mainComponents/MatchList.js
@@ -1,20 +1,35 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect, useCallback } from "react";
 import { Platform } from "react-native";
 import {
   View,
   Text,
   StyleSheet,
   FlatList,
-  ScrollView,
+  RefreshControl,
   ActivityIndicator,
   PlatformColor,
 } from "react-native";
+import { withSafeAreaInsets } from "react-native-safe-area-context";
 import { Context as BankrollContext } from "../../context/BankrollContext";
 
 import { ShowMatch } from "./ShowMatch";
 
 export const MatchList = () => {
+  const [refreshing, setRefreshing] = useState(false);
   const { state, getBets } = useContext(BankrollContext);
+
+  const wait = (timeout) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, timeout);
+    });
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    getBets();
+
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
   useEffect(() => {
     getBets();
@@ -25,6 +40,9 @@ export const MatchList = () => {
       <FlatList
         data={state.bets}
         keyExtractor={(bet) => bet.id.toString()}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
         renderItem={({ item }) => {
           return <ShowMatch bet={item} match={item.match} />;
         }}
