94575d0927c8cbe9104ed182943e018bedb518c1
qroux <quentin.roux@hotmail.fr>
2021-02-25 18:12:58 +0100

    BankrollScreen/Bankroll component updated ui

--- src/components/mainComponents/bankrollScreen/Bankroll.js
+++ src/components/mainComponents/bankrollScreen/Bankroll.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Common } from '../../../assets/common';
 import { Colors, Spacing, Size } from '../../../assets/main';
-import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons';
+import {
+  FontAwesome,
+  MaterialCommunityIcons,
+  Ionicons,
+  MaterialIcons,
+} from '@expo/vector-icons';
 
 import { BankrollCard } from './BankrollCard';
 import { navigationRef } from '../../../../RootNavigation';
@@ -18,70 +23,46 @@ export const Bankroll = ({ item }) => {
     .length;
   const success_rate = (success / positions.length) * 100;
 
-  const average_odds = () => {
-    let total_odds = 0;
-    let count = 0;
-
-    item.positions.forEach((position) => {
-      total_odds += position.bet.odds;
-      count += 1;
-    });
-
-    return count > 0 ? total_odds / count : '';
-  };
-
   return (
-    <View style={[Common.compContainer, Common.border, styles.width]}>
-      <View style={styles.header}>
-        <Text style={styles.title}>
-          {item.name}{' '}
-          <Text style={styles.details}>| {item.current_balance}€</Text>
-        </Text>
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate('BankrollDetail', {
-              bankroll_id: item._id,
-              bankroll_name: item.name,
-            })
-          }>
-          <MaterialCommunityIcons
-            name='playlist-edit'
-            size={24}
-            color='white'
-          />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.infoContainer}>
-        <View style={styles.rowContainer}>
-          <BankrollCard
-            title='Réussite'
-            icon={<FontAwesome name='trophy' size={18} color={'black'} />}
-            value={success_rate ? success_rate : '100'}
-            unit='%'
-          />
-          <BankrollCard
-            title='Cote moyenne'
-            icon={<FontAwesome name='line-chart' size={18} color={'black'} />}
-            value={average_odds()}
-            unit=' '
-          />
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate('BankrollDetail', {
+          bankroll_id: item._id,
+          bankroll_name: item.name,
+        })
+      }>
+      <View style={[Common.compContainer, Common.border, styles.width]}>
+        <View style={styles.header}>
+          <Text style={styles.title}>{item.name}</Text>
+          <Text style={styles.title}>{item.current_balance}€</Text>
         </View>
-        <View style={styles.rowContainer}>
-          <BankrollCard
-            title='Progression'
-            icon={<FontAwesome name='rocket' size={18} color={'black'} />}
-            value={progress}
-            unit='%'
-          />
-          <BankrollCard
-            title='Bénéfice'
-            icon={<FontAwesome name='money' size={18} color={'black'} />}
-            value={profits}
-            unit='€'
-          />
+        <View style={styles.infoContainer}>
+          <View style={styles.rowContainer}>
+            <BankrollCard
+              title='Nombre de paris'
+              icon={
+                <Ionicons name='ios-football-outline' size={18} color='black' />
+              }
+              value={positions.length}
+              unit=' '
+            />
+            <BankrollCard
+              title='Réussite'
+              icon={<FontAwesome name='trophy' size={18} color={'black'} />}
+              value={success_rate ? success_rate : '100'}
+              unit='%'
+            />
+            <BankrollCard
+              title='Bénéfice'
+              icon={<MaterialIcons name='euro' size={18} color={'black'} />}
+              value={profits}
+              unit='€'
+              evolution={progress}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
@@ -109,7 +90,6 @@ const styles = StyleSheet.create({
   },
   rowContainer: {
     flexDirection: 'row',
-    justifyContent: 'center',
-    paddingVertical: Spacing.small,
+    justifyContent: 'space-around',
   },
 });
--- src/components/mainComponents/bankrollScreen/BankrollCard.js
+++ src/components/mainComponents/bankrollScreen/BankrollCard.js
@@ -3,27 +3,36 @@ import { Text, View, StyleSheet } from 'react-native';
 import { Common } from '../../../assets/common';
 import { Size, Colors } from '../../../assets/main';
 
-export const BankrollCard = ({ title, icon, value, unit }) => (
-  <View style={[styles.container]}>
-    <View style={styles.title}>
-      {icon}
-      <Text style={styles.title}>{title}</Text>
+export const BankrollCard = ({ title, icon, value, unit, evolution }) => {
+  const renderProgress = () => {
+    const sign = evolution >= 0 ? '+' : '-';
+
+    return evolution ? `(${sign}${evolution}%)` : null;
+  };
+  return (
+    <View style={[styles.container]}>
+      <View style={styles.title}>
+        {icon}
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      <Text
+        style={[
+          styles.content,
+          { color: value >= 0 ? Colors.success : Colors.fail },
+        ]}>
+        {value}
+        {unit ? unit : null}
+        {renderProgress()}
+      </Text>
     </View>
-    <Text
-      style={[
-        styles.content,
-        { color: value >= 0 ? Colors.success : Colors.fail },
-      ]}>
-      {value}
-      {unit}
-    </Text>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    width: 160,
     alignItems: 'center',
+    justifyContent: 'center',
+    flex: 0.33,
   },
   title: {
     fontWeight: 'normal',
@@ -32,7 +41,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   content: {
-    fontWeight: 'bold',
-    fontSize: Size.regular,
+    fontSize: Size.small,
   },
 });
