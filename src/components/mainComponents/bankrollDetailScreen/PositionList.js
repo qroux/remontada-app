1751aad4c4e1229238de438c3a78a12ab2935fc3
qroux <quentin.roux@hotmail.fr>
2021-02-26 17:36:24 +0100

    bankrolldetailScreen state.isLoading when loading positions

--- src/components/mainComponents/bankrollDetailScreen/PositionList.js
+++ src/components/mainComponents/bankrollDetailScreen/PositionList.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View, FlatList, StyleSheet } from 'react-native';
 import { Size } from '../../../assets/main';
+import { Context as BankrollContext } from '../../../context/BankrollContext';
 
 import { Position } from './Position';
 
-export const PositionList = ({ positions }) => (
-  <View>
-    <Text style={styles.header}>Derniers paris enregistrés</Text>
-    <FlatList
-      data={positions}
-      keyExtractor={(position) => position._id}
-      renderItem={({ item }) => <Position position={item} />}
-    />
-  </View>
-);
+export const PositionList = ({ positions }) => {
+  const { state } = useContext(BankrollContext);
+
+  return (
+    <View>
+      <Text style={styles.header}>Derniers paris enregistrés</Text>
+      {state.isLoading ? null : (
+        <FlatList
+          data={positions}
+          keyExtractor={(position) => position._id}
+          renderItem={({ item }) => <Position position={item} />}
+        />
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   header: {
