fb74128caece4751afc44c9cd4206a11041de5ae
qroux <quentin.roux@hotmail.fr>
2021-02-26 17:25:12 +0100

    StatusbarDark added on MainStack/PronosticScreen to set default statusBar when user already loged in

--- src/screens/PronosticScreen.js
+++ src/screens/PronosticScreen.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  ImageBackground,
-  Dimensions,
-  SafeAreaView,
-} from 'react-native';
-import { Colors, Spacing, Size } from '../assets/main';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import { Common } from '../assets/common';
 
+import { StatusBarDark } from '../components/shared/StatusBarDark';
 import { MatchList } from '../components/mainComponents/pronosticScreen/MatchList';
 
 export const PronosticScreen = () => {
   return (
     <SafeAreaView style={Common.fullPage}>
+      <StatusBarDark />
       <View style={Common.container}>
         <Text style={Common.title}>À l'affiche</Text>
         <MatchList />
